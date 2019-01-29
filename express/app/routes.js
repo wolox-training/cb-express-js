221e2e4871eacdf35fae8075655275d88f667b8e
carlos <carlosmbollero@gmail.com>
2019-01-29 15:14:41 -0300

    add users post method endpoint

--- express/app/routes.js
+++ express/app/routes.js
@@ -1,6 +1,7 @@
-// const controller = require('./controllers/controller');
+const users = require('./controllers/users');
 
 exports.init = app => {
+  app.post('/users', [], users.create);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
