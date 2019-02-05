0e7efc5e86ce2bd664d3065c624c48e9baae2be7
carlos <carlosmbollero@gmail.com>
2019-02-04 23:06:53 -0300

    move email existance check from servico into controller

--- express/app/controllers/users.js
+++ express/app/controllers/users.js
@@ -1,24 +1,32 @@
-const userService = require('../services/userService');
-const logger = require('../logger');
-const bcrypt = require('bcryptjs');
+const user = require('../models').user,
+  userService = require('../services/userService'),
+  errors = require('../errors'),
+  logger = require('../logger'),
+  bcrypt = require('bcryptjs');
 
 const encryptPassword = ({ password }) => {
   const salt = bcrypt.genSaltSync();
   return bcrypt.hash(password, salt);
 };
 
-exports.create = (req, res) =>
-  encryptPassword(req.body).then(hashedPassword => {
-    const fields = { ...req.body, password: hashedPassword };
-    return userService
-      .create(fields)
-      .then(newUser => {
-        const user = { id: newUser.id, firstName: newUser.firstName, lastName: newUser.lastName };
-        logger.info(`The user ${user} was successfully created`);
-        res.status(201).send(user);
-      })
-      .catch(error => {
-        logger.error(`Failed to create user. ${error}`);
-        res.status(400).send(error.toString());
+exports.create = (req, res, next) =>
+  user.findOne({ where: { email: req.body.email } }).then(userInstances => {
+    if (userInstances) {
+      next(errors.emailAlreadyExists('The email already exists'));
+    } else {
+      return encryptPassword(req.body).then(hashedPassword => {
+        const fields = { ...req.body, password: hashedPassword };
+        return userService
+          .create(fields)
+          .then(newUser => {
+            const User = { id: newUser.id, firstName: newUser.firstName, lastName: newUser.lastName };
+            logger.info(`The user ${User} was successfully created`);
+            res.status(201).send(user);
+          })
+          .catch(error => {
+            logger.error(`Failed to create user. ${error}`);
+            res.status(400).send(error.toString());
+          });
       });
+    }
   });
--- express/app/services/userService.js
+++ express/app/services/userService.js
@@ -1,10 +1,3 @@
 const user = require('../models').user;
 
-exports.create = async userFields =>
-  user.findOne({ where: { email: userFields.email } }).then(userInstance => {
-    if (userInstance) {
-      throw new Error('Email already exists');
-    } else {
-      return user.create(userFields);
-    }
-  });
+exports.create = userFields => user.create(userFields);
