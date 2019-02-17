172627b380eee33124458c8ff0be72b56e1cd6d0
carlos <carlosmbollero@gmail.com>
2019-02-17 20:55:10 -0300

    add new route, controller and errors

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,12 @@
 const users = require('./controllers/users'),
   userFieldsValidator = require('./middlewares/userFieldsValidator').handle,
-  userLoginValidator = require('./middlewares/userLoginValidator').handle;
+  userLoginValidator = require('./middlewares/userLoginValidator').handle,
+  authValidator = require('./middlewares/authValidator').handle;
 
 exports.init = app => {
   app.post('/users', userFieldsValidator, users.create);
   app.post('/users/sessions', userLoginValidator, users.logIn);
+  app.get('/users', authValidator, users.list);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
 };
